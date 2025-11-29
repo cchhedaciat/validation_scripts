97a066e782a45ccebdce0d33155486915dc3f086
Chetan Chheda <cchheda@ciat.edu>
2025-11-29 17:59:17 +0100

    Added comment

--- 1-1p2.sh
+++ 1-1p2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# The lab needs to be updated to either remove rhgb from the boot menu OR update #10 to include --update-bls-cmdline
 grep -q rhgb /proc/cmdline
 if [ $? -ne 0 ]
 then 
